a378d26eff1b99600507e28fdd13b781f9fb000e
Hanjing <hanjinglin8@gmail.com>
2026-07-15 17:35:37 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int[]sums = new int[2];
+        
+        for(int i = 0; i<nums.length; i++){
+            for(int j = i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target && i!=j){
+                    sums[0] = i;
+                    sums[1] = j;
+                }else{
+                    continue;
+                }
+            }
+        }
+
+        return sums;
+
+    }
+}
